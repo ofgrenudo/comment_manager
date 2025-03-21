ce2e04adeb93b5d20d16d7c0e59c129bd81f28da
Joshua <joshuawintersbrown@gmail.com>
2025-03-21 07:50:28 -0400

    working on validate_comment function to take out alot of repeatability

--- src/sqlite.rs
+++ src/sqlite.rs
@@ -1,4 +1,8 @@
-use std::error::Error;
+use std::{
+    error::Error,
+    io,
+    path::{self, Path},
+};
 
 use async_trait::async_trait;
 use sqlite::Connection;
@@ -11,6 +15,16 @@ pub struct SQLite {
     pub conn: Connection,
 }
 
+const DATABASE_INIT_STRING: &str = "CREATE TABLE IF NOT EXISTS comments (
+                    id TEXT NOT NULL PRIMARY KEY,
+                    ip TEXT,
+                    username TEXT NOT NULL,
+                    comment TEXT NOT NULL,
+                    timestamp TEXT,
+                    visible INT NOT NULL,
+                    post_url TEXT
+                );";
+
 #[async_trait]
 impl Database for SQLite {
     fn connect(connection_information: &str) -> Result<SQLite, Box<dyn Error>> {
@@ -21,18 +35,23 @@ impl Database for SQLite {
             connection_string: connection_information.to_string(),
         };
 
-        // 1. Initialize database.
+        // 2 Initialize database.
         Database::initialize_database(&connection_information_modified)?;
 
-        // 2. Return database connection.
+        // 3. Return database connection.
         Ok(connection_information_modified)
     }
 
     fn initialize_database(&self) -> Result<(), Box<dyn Error>> {
-        // 1. Check if database file exists.
-        // 2. Check if table exists in database.     
-        // 3. Return status of initialization.   
-        todo!();
+        // 1. Connect to database.
+        // NOTE Connection::open from the connect method will create the file if it does not exist!
+        let database: &Connection = &self.conn;
+
+        // 2. Initialize database.
+        database.execute(DATABASE_INIT_STRING.to_string())?;
+
+        // 3. Return status of intialization.
+        Ok(())
     }
 
     // Simple CRUD
@@ -42,7 +61,7 @@ impl Database for SQLite {
         // 3. Return status of comment creation
         todo!();
     }
-    
+
     fn read_comment(&self, id: Uuid) -> Result<(), Box<dyn Error>> {
         // 1. Validate ID.
         // 2. Connect to database.
@@ -50,11 +69,11 @@ impl Database for SQLite {
         // 4. Return comment.
         todo!();
     }
-    
+
     fn read_comments(&self, comment: Comment) -> Result<(), Box<dyn Error>> {
         // 1. Validate Comment contents.
         // 2. Connect to database.
-        // 3. Query for comments LIKE the comment provided. 
+        // 3. Query for comments LIKE the comment provided.
         // 4. Return array of comments to user.
         todo!();
     }
@@ -66,12 +85,39 @@ impl Database for SQLite {
         // 4. Return status of update.
         todo!();
     }
-    
+
     fn delete_comment(&self, comment: Comment) -> Result<(), Box<dyn Error>> {
         // 1. Validate Comment contents.
         // 2. Connect to database.
         // 3. Flip visibility to 0. Do not actually delete the comment.
         // 4. Return status of deletion.
         todo!();
+    }
+
+    // NOTE: Not sure what im doing wrong here. Trying to return a specific error. this type of shit is why i just make a custom type, and return the error that way instead of using rust native shit.
+    fn validate_comment(&self, comment: Comment) -> Result<(), Box<dyn Error>> {
+        // 1. Validate comment contents.
+        comment.into_iter().for_each(|item| {
+            if item.len() > 10000 {
+                return Err(Box::new(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Comment is too long",
+                )));
+            } else {
+                return Ok(());
+            }
+        });
+
+        comment.into_iter().for_each(|item| {
+            if item.is_empty() {
+                return Err(Box::new(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Item cannot be empty",
+                )));
+            }
+        });
+
+        // 2. Return status of validation.
+        Ok(())
     }
 }
